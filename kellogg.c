82764ac6739763d6b3b97550570b5ffdd5de8436
Cody K <Z_10535969@cs3060.ad.uvu.edu>
2015-11-10 18:29:22 -0700

    created kellogg.c

---
+++ kellogg.c
@@ -0,0 +1,12 @@
+//Code writer: Cody Kellogg / Instructor
+//Group project
+//CS 3060-601 Fall 2015
+//Assignment #6
+
+/* Promise of Originality
+I promise that this source code file has, in it's entirety, been
+written by myself and by no other person or persons. If at any time an
+exact copy of this source code is found to be used by another person in
+this term, I understand that both myself and the student that submitted
+the copy will receive a zero on this assignment.
+*/
